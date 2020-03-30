21d3b77cdcdbb560a6a878e41f3d3c30411d46fe
phillowcompiler <49435323+phillowcompiler@users.noreply.github.com>
2020-03-31 00:18:50 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,554 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 2 
+2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 2 
+2 3 3 3 1 1 3 3 3 3 1 1 3 3 3 2 
+2 3 3 1 1 1 1 3 3 1 1 1 1 3 3 2 
+2 3 1 1 1 1 1 1 1 1 1 1 1 1 3 2 
+2 3 1 1 1 1 1 1 1 1 1 1 1 1 3 2 
+2 3 1 1 1 1 1 1 1 1 1 1 1 1 3 2 
+2 3 3 1 1 1 1 1 1 1 1 1 1 3 3 2 
+2 3 3 3 1 1 1 1 1 1 1 1 3 3 3 2 
+2 3 3 3 3 1 1 1 1 1 1 3 3 3 3 2 
+2 3 3 3 3 3 1 1 1 1 3 3 3 3 3 2 
+2 3 3 3 3 3 3 1 1 3 3 3 3 3 3 2 
+2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 2 
+2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 9 9 9 9 9 9 9 9 9 9 9 9 9 9 8 
+8 9 9 9 9 9 9 9 9 9 9 9 9 9 9 8 
+8 9 9 9 9 9 9 9 8 8 8 9 9 9 9 8 
+8 9 9 9 9 9 9 8 8 8 9 9 9 9 9 8 
+8 9 9 9 9 8 8 8 8 8 9 9 9 9 9 8 
+8 9 9 9 8 8 8 8 8 8 8 9 9 8 9 8 
+8 9 9 8 8 6 8 8 8 8 8 8 9 8 9 8 
+8 9 8 8 8 1 8 8 8 8 8 8 8 6 9 8 
+8 9 9 8 8 8 8 8 8 8 8 8 6 8 9 8 
+8 9 9 9 8 8 8 8 8 8 8 9 9 8 9 8 
+8 9 9 9 9 8 8 8 8 6 9 9 9 9 9 8 
+8 9 9 9 9 9 9 9 9 9 9 9 9 9 9 8 
+8 9 9 9 9 9 9 9 9 9 9 9 9 9 9 8 
+8 9 9 9 9 9 9 9 9 9 9 9 9 9 9 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 7 7 7 7 7 7 1 1 7 7 7 7 7 7 6 
+6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
+6 7 7 7 1 1 7 7 7 7 1 1 7 7 7 6 
+6 7 7 1 1 1 1 7 7 1 1 1 1 7 7 6 
+6 7 7 1 1 1 1 1 1 1 1 1 1 7 7 6 
+6 7 7 7 1 1 7 1 1 7 1 1 7 7 7 6 
+6 7 1 7 7 1 1 6 6 1 1 7 7 1 7 6 
+6 7 1 7 7 1 1 6 6 1 1 7 7 1 7 6 
+6 7 7 7 1 1 7 1 1 7 1 1 7 7 7 6 
+6 7 7 1 1 1 1 1 1 1 1 1 1 7 7 6 
+6 7 7 1 1 1 1 7 7 1 1 1 1 7 7 6 
+6 7 7 7 1 1 7 7 7 7 1 1 7 7 7 6 
+6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
+6 7 7 7 7 7 7 1 1 7 7 7 7 7 7 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+e e e e e e e e e e e e e e e e 
+e 2 2 2 2 2 2 2 4 4 4 4 4 4 4 e 
+e 2 2 2 2 2 2 2 4 4 4 4 4 4 4 e 
+e 2 2 2 2 2 2 2 4 4 4 4 4 4 4 e 
+e 2 2 2 2 2 2 2 4 4 4 4 4 4 4 e 
+e 2 2 2 2 2 2 2 1 1 1 4 4 4 4 e 
+e 2 2 2 2 2 2 2 1 3 1 4 4 4 4 e 
+e 2 2 2 2 2 2 2 1 1 1 4 4 4 4 e 
+e 4 4 4 4 1 1 1 2 2 2 2 2 2 2 e 
+e 4 4 4 4 1 3 1 2 2 2 2 2 2 2 e 
+e 4 4 4 4 1 1 1 2 2 2 2 2 2 2 e 
+e 4 4 4 4 4 4 4 2 2 2 2 2 2 2 e 
+e 4 4 4 4 4 4 4 2 2 2 2 2 2 2 e 
+e 4 4 4 4 4 4 4 2 2 2 2 2 2 2 e 
+e 4 4 4 4 4 4 4 2 2 2 2 2 2 2 e 
+e e e e e e e e e e e e e e e e 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
+4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
+4 5 5 5 b 5 5 5 5 5 5 b 5 5 5 4 
+4 5 5 b 1 b 5 5 5 5 b 1 b 5 5 4 
+4 5 5 b 1 b 5 5 5 5 b 1 b 5 5 4 
+4 5 b 1 1 1 b b b b 1 1 1 b 5 4 
+4 5 b 1 1 1 1 1 1 1 1 1 1 b 5 4 
+4 5 b 1 1 b 1 1 1 1 b 1 1 b 5 4 
+4 5 b 1 1 b 1 1 1 1 b 1 1 b 5 4 
+4 5 b 1 1 1 1 1 1 1 1 1 1 b 5 4 
+4 5 b 1 1 1 1 b b 1 1 1 1 b 5 4 
+4 5 5 b 1 1 1 1 1 1 1 1 b 5 5 4 
+4 5 5 5 b b b b b b b b 5 5 5 4 
+4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+. . . . . . . . . . . . . . . . 
+9 9 9 9 9 . 9 . . . 9 . 9 9 9 9 
+9 . . . . . 9 9 . . 9 . 9 . . . 
+9 9 9 9 9 . 9 . 9 . 9 . 9 . . . 
+9 . . . . . 9 . 9 . 9 . 9 . 9 9 
+9 . . . . . 9 . . 9 9 . 9 . . 9 
+9 9 9 9 9 . 9 . . . 9 . 9 9 9 9 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (posx >= 5 && posx <= 7 && posy >= 2 && posy <= 4) {
+        index22 = (posy - 2) * 3 + (posx - 5)
+        if (0 <= flgChangeTile) {
+            if (flgChangeTile != index22) {
+                tmptile = lstPlayTiles[index22]
+                lstPlayTiles[index22] = lstPlayTiles[flgChangeTile]
+                lstPlayTiles[flgChangeTile] = tmptile
+                mySprite.say("Change!", 500)
+                dispTile(posx, posy, lstPlayTiles[index22])
+                dispTile(flgChangeTile % 3 + 5, Math.floor(flgChangeTile / 3) + 2, tmptile)
+                hilite.startEffect(effects.fire, 500)
+                sprChangeFrom.startEffect(effects.fire, 500)
+            }
+            flgChangeTile = -1
+            sprChangeFrom.setFlag(SpriteFlag.Invisible, true)
+            chkGameWin()
+        } else {
+            flgChangeTile = index22
+            mySprite.say("From", 500)
+            sprChangeFrom.setPosition(posx * 16 + 8, posy * 16 + 8)
+            sprChangeFrom.setFlag(SpriteFlag.Invisible, false)
+        }
+    }
+})
+function chkGameWin () {
+    index3 = 0
+    while (index3 >= 0 && index3 < 9) {
+        tmptile = lstPlayTiles[index3] % 10
+        if (tmptile == lstBaseTiles[index3] % 10) {
+            index3 += 1
+        } else {
+            index3 = -1
+        }
+    }
+    if (index3 >= 9) {
+        for (let index = 0; index <= 8; index++) {
+            dispTile(index % 3 + 5, Math.floor(index / 3) + 2, lstPlayTiles[index])
+        }
+        music.baDing.play()
+        effects.smiles.startScreenEffect(2000)
+        pause(2000)
+        game.over(true, effects.confetti)
+    }
+}
+function makeTiles () {
+    tileList = [0, 1, 2, 3, 4, 5]
+    lstTilePatternID = []
+    while (lstTilePatternID.length < 6) {
+        rnd = Math.randomRange(1, tileList.length) - 1
+        lstTilePatternID.push(tileList[rnd])
+        tmptile = tileList.removeAt(rnd)
+    }
+    initTile(true)
+    initTile(false)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    mySprite.startEffect(effects.bubbles, 500)
+    music.baDing.play()
+    valEng = 90
+    imgEng.fillRect(0, 0, valEng, 8, 7)
+    mySprite.say(":)", 1000)
+    catPosChg()
+})
+function reverse (tile: number) {
+    tmptile = tile % 10 * 10 + Math.floor(tile / 10)
+}
+function initTile (base: boolean) {
+    tileList = [14, 15, 16, 24, 25, 26, 34, 35, 36]
+    if (base) {
+        lstBaseTiles = []
+    } else {
+        lstPlayTiles = []
+    }
+    while (tileList.length > 0) {
+        rnd = Math.randomRange(1, tileList.length) - 1
+        tmptile = tileList[rnd]
+        if (Math.randomRange(1, 2) < 2) {
+            reverse(tmptile)
+        }
+        if (base) {
+            lstBaseTiles.push(tmptile)
+        } else {
+            lstPlayTiles.push(tmptile)
+        }
+        tmptile = tileList.removeAt(rnd)
+    }
+    for (let index2 = 0; index2 <= 8; index2++) {
+        if (base) {
+            dispTile(index2 % 3, Math.floor(index2 / 3) + 2, lstBaseTiles[index2])
+        } else {
+            dispTile(index2 % 3 + 5, Math.floor(index2 / 3) + 2, lstPlayTiles[index2])
+        }
+    }
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    index22 = (posy - 2) * 3 + (posx - 5)
+    tmptile = lstPlayTiles[index22]
+    reverse(tmptile)
+    lstPlayTiles[index22] = tmptile
+    mySprite.say("Reverse", 500)
+    hilite.startEffect(effects.fountain, 500)
+    dispTile(posx, posy, tmptile)
+    chkGameWin()
+})
+function dispTile (col: number, row: number, idx: number) {
+    tiles.setTileAt(tiles.getTileLocation(col, row), tilepatternList[lstTilePatternID[idx % 10 - 1]])
+}
+function catPosChg () {
+    catTimer = 10
+    oldCatPos = catPos
+    rnd = Math.randomRange(1, lstCatPos.length)
+    catPos = lstCatPos.removeAt(rnd)
+    lstCatPos.push(oldCatPos)
+    mySprite2.setPosition(Math.floor(catPos / 10) * 16 + 8, catPos % 10 * 16 + 8)
+}
+function getPosition () {
+    controller.moveSprite(mySprite, 80, 80)
+    posx = Math.floor(mySprite.x / 16)
+    posy = Math.floor(mySprite.y / 16)
+    if (posx >= 5 && posx <= 7 && posy >= 2 && posy <= 4) {
+        hilite.setPosition(posx * 16 + 8, posy * 16 + 8)
+        hilite.setFlag(SpriteFlag.Invisible, false)
+    } else {
+        hilite.setFlag(SpriteFlag.Invisible, true)
+    }
+}
+let flgtimer = 0
+let oldCatPos = 0
+let rnd = 0
+let lstTilePatternID: number[] = []
+let tileList: number[] = []
+let lstBaseTiles: number[] = []
+let index3 = 0
+let lstPlayTiles: number[] = []
+let tmptile = 0
+let index22 = 0
+let posy = 0
+let posx = 0
+let imgEng: Image = null
+let valEng = 0
+let flgChangeTile = 0
+let mySprite: Sprite = null
+let sprChangeFrom: Sprite = null
+let catPos = 0
+let lstCatPos: number[] = []
+let mySprite2: Sprite = null
+let hilite: Sprite = null
+let tilepatternList: Image[] = []
+let catTimer = 0
+let list = 0
+catTimer = 0
+tiles.setTilemap(tiles.createTilemap(
+            hex`10001000000000050606060606070000000000000000000915151515150f0000000000000000000915020202150f0000000000000000000915020202150f0000000000000000000915020202150f0000000000000000000915151515150f000000000000000000101212121212110000000000000000001b000000000000000000000000161718191a020000000000000000000000001c00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            img`
+. . . 2 2 2 2 2 2 2 . . . . . . 
+. . . 2 . . . . . 2 . . . . . . 
+. . . 2 . . . . . 2 . . . . . . 
+. . . 2 . . . . . 2 . . . . . . 
+. . . 2 . . . . . 2 . . . . . . 
+. . . 2 . . . . . 2 . . . . . . 
+. . . 2 2 2 2 2 2 2 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.dungeon.chestClosed,sprites.dungeon.collectibleInsignia,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterWest0,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterEast1,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.floorMixed,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.darkGroundCenter,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,sprites.castle.tileGrass2],
+            TileScale.Sixteen
+        ))
+tilepatternList = [myTiles.tile1, myTiles.tile2, myTiles.tile3, myTiles.tile4, myTiles.tile5, sprites.dungeon.collectibleInsignia]
+hilite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Projectile)
+animation.runImageAnimation(
+hilite,
+[img`
+1 1 1 1 . . . . . . . . 1 1 1 1 
+1 . 1 . . . . . . . . . . 1 . 1 
+1 1 . . . . . . . . . . . . 1 1 
+1 . . . . . . . . . . . . . . 1 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+1 . . . . . . . . . . . . . . 1 
+1 1 . . . . . . . . . . . . 1 1 
+1 . 1 . . . . . . . . . . 1 . 1 
+1 1 1 1 . . . . . . . . 1 1 1 1 
+`,img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`],
+500,
+true
+)
+hilite.setFlag(SpriteFlag.Invisible, true)
+mySprite2 = sprites.create(img`
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f d d d d f d e . b f b 
+f d d f d d f d d f . f d f 
+f b d d b b d d 2 f . f d f 
+. f 2 2 2 2 2 2 b b f f d f 
+. f b d d d d d d b b d b f 
+. f d d d d d b d d f f f . 
+. f d f f f d f f d f . . . 
+. f f . . f f . . f f . . . 
+`, SpriteKind.Food)
+animation.runImageAnimation(
+mySprite2,
+[img`
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f d d d d f d e . b f b 
+f d d f d d f d d f . f d f 
+f b d d b b d d 2 f . f d f 
+. f 2 2 2 2 2 2 b b f f d f 
+. f b d d d d d d b b d b f 
+. f d d d d d b d d f f f . 
+. f d f f f d f f d f . . . 
+. f f . . f f . . f f . . . 
+`,img`
+. . . . . . . . . . . . . . 
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f d d d d f d e b f b . 
+f d d f d d f d d f f d f . 
+f b d d b b d d 2 b f d f . 
+. f 2 2 2 2 2 2 d b d b f . 
+. f d d d d d d d f f f . . 
+. f d b d f f f d f . . . . 
+. . f f f f . . f f . . . . 
+`],
+500,
+true
+)
+mySprite2.setPosition(105, 20)
+let mySprite3 = sprites.create(img`
+. . . . . . . . . . . . . . 
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f d d d d f d e . b f b 
+f d d f d d f d d f . f d f 
+f b d d b b d d 2 b f f d f 
+. f 2 2 2 2 2 2 d b b d b f 
+. f d d d d d d d f f f f . 
+. . f d b d f d f . . . . . 
+. . . f f f f f f . . . . . 
+`, SpriteKind.Projectile)
+mySprite3.setPosition(24, 24)
+lstCatPos = [41, 51, 61, 71, 81, 42, 82, 43, 83, 44, 84, 45, 55, 65, 75]
+catPos = 85
+sprChangeFrom = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+animation.runImageAnimation(
+sprChangeFrom,
+[img`
+. . . . . f f f f f f . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . f f f 1 1 1 1 f f f . . . 
+. . . f 1 1 1 1 1 1 1 1 f . . . 
+. . . . f 1 1 1 1 1 1 f . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . . . f 1 1 f . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . f f f 1 1 1 1 f f f . . . 
+. . . f 1 1 1 1 1 1 1 1 f . . . 
+. . . . f 1 1 1 1 1 1 f . . . . 
+. . . . . f 1 1 1 1 f . . . . . 
+. . . . . . f 1 1 f . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`],
+500,
+true
+)
+sprChangeFrom.setFlag(SpriteFlag.Invisible, true)
+mySprite = sprites.create(img`
+. . . . . . 5 . 5 . . . . . . . 
+. . . . . f 5 5 5 f f . . . . . 
+. . . . f 1 5 2 5 1 6 f . . . . 
+. . . f 1 6 6 6 6 6 1 6 f . . . 
+. . . f 6 6 f f f f 6 1 f . . . 
+. . . f 6 f f d d f f 6 f . . . 
+. . f 6 f d f d d f d f 6 f . . 
+. . f 6 f d 3 d d 3 d f 6 f . . 
+. . f 6 6 f d d d d f 6 6 f . . 
+. f 6 6 f 3 f f f f 3 f 6 6 f . 
+. . f f d 3 5 3 3 5 3 d f f . . 
+. . f d d f 3 5 5 3 f d d f . . 
+. . . f f 3 3 3 3 3 3 f f . . . 
+. . . f 3 3 5 3 3 5 3 3 f . . . 
+. . . f f f f f f f f f f . . . 
+. . . . . f f . . f f . . . . . 
+`, SpriteKind.Player)
+flgChangeTile = -1
+valEng = 96
+imgEng = image.create(90, 4)
+imgEng.fillRect(0, 0, valEng, 8, 7)
+let sprEng = sprites.create(imgEng, SpriteKind.Player)
+sprEng.setPosition(112, 116)
+makeTiles()
+game.onUpdateInterval(500, function () {
+    if (valEng > 0) {
+        if (valEng == 20) {
+            imgEng.fillRect(0, 0, valEng, 8, 2)
+        }
+        valEng += -1
+        imgEng.fillRect(valEng, 0, 90 - valEng, 8, 15)
+        if (valEng == 0) {
+            game.over(false, effects.melt)
+        }
+    }
+    catTimer += -1
+    if (catTimer < 0) {
+        catPosChg()
+    }
+})
+forever(function () {
+    getPosition()
+})
+game.onUpdateInterval(1000, function () {
+    if (flgtimer > 0) {
+        mySprite3.say("Please")
+        flgtimer = 0
+    } else {
+        mySprite3.say("Arrange")
+        flgtimer = 1
+    }
+})
